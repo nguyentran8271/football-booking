34f2937cc76f80a76771277aebbb904d6d4736b1
nguyentran8271 <nguyentran8271@users.noreply.github.com>
2026-04-12 00:23:14 +0700

    fix: add image_url accessor to FeaturedField model

--- app/Models/FeaturedField.php
+++ app/Models/FeaturedField.php
@@ -22,4 +22,12 @@ class FeaturedField extends Model
         'price' => 'float',
         'order' => 'integer'
     ];
+
+    public function getImageUrlAttribute(): string
+    {
+        if (!$this->image) return asset('images/default-field.jpg');
+        if (str_starts_with($this->image, 'http')) return $this->image;
+        if (str_starts_with($this->image, 'images/')) return asset($this->image);
+        return asset('storage/' . $this->image);
+    }
 }
